cc7342a531d4d7c6e1635a45c4c121780d1782b5
oguzkaganozt <oguz@leodrive.ai>
2024-01-31 17:00:28 +0300

    Fix docker setup and add print help to setup script

--- setup-dev-env.sh
+++ setup-dev-env.sh
@@ -5,6 +5,24 @@
 
 set -e
 
+# Function to print help message
+print_help() {
+    echo "Usage: setup-dev-env.sh [OPTIONS]"
+    echo "Options:"
+    echo "  --help          Display this help message"
+    echo "  -h              Display this help message"
+    echo "  -y              Use non-interactive mode"
+    echo "  -v              Enable debug outputs"
+    echo "  --no-nvidia     Disable installation of the NVIDIA-related roles ('cuda' and 'tensorrt')"
+    echo "  --no-cuda-drivers Disable installation of 'cuda-drivers' in the role 'cuda'"
+    echo "  --runtime       Disable installation dev package of role 'cuda' and 'tensorrt'"
+    echo "  --data-dir      Set data directory (default: $HOME/autoware_data)"
+    echo "  --download-artifacts"
+    echo "                  Download artifacts"
+    echo "  --module        Specify the module (default: all)"
+    echo ""
+}
+
 SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
 
 # Parse arguments
@@ -13,6 +31,10 @@ option_data_dir="$HOME/autoware_data"
 
 while [ "$1" != "" ]; do
     case "$1" in
+    --help | -h)
+        print_help
+        exit 1
+        ;;
     -y)
         # Use non-interactive mode.
         option_yes=true
@@ -42,10 +64,6 @@ while [ "$1" != "" ]; do
         # Set download artifacts option
         option_download_artifacts=true
         ;;
-    --docker)
-        # Set download artifacts option
-        option_docker=true
-        ;;
     --module)
         option_module="$2"
         shift
@@ -59,12 +77,6 @@ done
 
 # Select installation type
 target_playbook="autoware.dev_env.universe" # default
-
-# Set-up docker environment
-if [ "$option_docker" = "true" ]; then
-    target_playbook="autoware.dev_env.docker"
-    ansible_args+=("--extra-vars" "cuda_install_drivers=true")
-fi
 
 if [ ${#args[@]} -ge 1 ]; then
     target_playbook="autoware.dev_env.${args[0]}"
